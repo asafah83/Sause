36d77dab80e782d14689caaafd371304e16ed0c2
asafah83 <asafah83@gmail.com>
2025-01-16 10:00:15 +0200

    Refactored BuyProductTest to reduce repetition and improve maintainability

--- src/test/java/tests/BuyProductTest.java
+++ src/test/java/tests/BuyProductTest.java
@@ -1,32 +1,42 @@
 package tests;
 
 import io.qameta.allure.Description;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.*;
 
 public class BuyProductTest extends BaseTest {
+    private LoginPage lp;
+    private ProductsPage productsPage;
+
     // WebDriver driver;
     //   public static void main(String[] args) throws InterruptedException {
 //        WebDriver driver = new ChromeDriver();
 //        driver.manage().window().maximize();
 //        driver.get("https://www.saucedemo.com/");
+    @BeforeMethod
+    public void setUpProductsPages() {
+        // Initialize commonly used pages
+        lp = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+    }
+
+    private void loginAsStandardUser() throws InterruptedException {
+        lp.login("standard_user", "secret_sauce");
+        productsPage.verifyPageTitle("Products"); // Verify login success
+    }
 
     @Test
-    @Description("Test 1 - Login to the website and verify its the correct page")
+    @Description("Test 1 - Login with a valid user, to the website and verify that the user is in correct page")
     public void tc1Negative() throws InterruptedException {
-        LoginPage lp = new LoginPage(driver);
-        lp.login("standard_user", "secret_sauce");
-        ProductsPage productsPage = new ProductsPage(driver);
-        productsPage.verifyPageTitle("Products");
+        loginAsStandardUser();
     }
 
     @Test
     @Description("An E2E test: login > select product > add to cart > buy the product")
     public void tc2PositiveTestE2E() throws InterruptedException {
-        LoginPage lp = new LoginPage(driver);
-        lp.login("standard_user", "secret_sauce");
+        loginAsStandardUser();
 
-        ProductsPage productsPage = new ProductsPage(driver);
         productsPage.selectProduct("Sauce Labs Fleece Jacket");
 
         ProductPage productPage = new ProductPage(driver);
@@ -57,18 +67,15 @@ public class BuyProductTest extends BaseTest {
     @Description("E2E Test - Add a product to the cart, directly, from the Products page > complete the flow")
     public void tc4_addProductToCartFromProductsPage() throws InterruptedException {
         LoginPage lp = new LoginPage(driver);
-        lp.login("standard_user", "secret_sauce");
-        ProductsPage productsPage = new ProductsPage(driver);
+        loginAsStandardUser();
         productsPage.addToCart("Sauce Labs Onesie");
-
     }
 
     @Test
     @Description("This test check the Sort functionality")
     public void tc3Sorting() throws InterruptedException {
         LoginPage lp = new LoginPage(driver);
-        lp.login("standard_user", "secret_sauce");
-        ProductsPage productsPage = new ProductsPage(driver);
+        loginAsStandardUser();
         productsPage.sortByOption("Price (low to high)");
     }
 }
